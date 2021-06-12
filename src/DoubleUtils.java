4aabb95c54801f5b4d09b41b33aba10e4ab2f7aa
Sharron Jamie <sjamie@il-e1.msk.21-school.ru>
2021-06-12 21:02:11 +0300

    push

--- src/DoubleUtils.java
+++ src/DoubleUtils.java
@@ -4,7 +4,25 @@ public class DoubleUtils {
         return d % 1 == 0 ? String.valueOf((long) d) : String.format("%.3f", d);
     }
 
-    public static double sqrt(double d) {
-        return Math.sqrt(d);
+    public static double sqrt(double x) {
+        double epsilon;
+        double guess;
+
+        guess = x;
+        epsilon = 1e-15;
+        while (abs(guess - x / guess) > epsilon * guess) {
+            guess = (x / guess + guess) / 2.0;
+        }
+        if (guess != Math.sqrt(x)) {
+            System.out.println("fddffddf");
+            throw new RuntimeException("fddffddf");
+        }
+        return guess;
     }
+
+    public static double abs(double a) {
+        return (a <= 0.0D) ? 0.0D - a : a;
+    }//FFFF=1
 }
+
+//5*X^0 + 4*X + -9.3 * X^2=1*X^0
